ad7e79c2a40e8f56148b3bbb881e3450a979878b
sαɴɖεερ sʜαяɱα <sandeepshrmadev@gmail.com>
2025-05-03 22:54:50 +0530

    Update facial_recognition.py

--- RaOne/visual_interface/facial_recognition.py
+++ RaOne/visual_interface/facial_recognition.py
@@ -1,1 +1,22 @@
+import cv2
 
+def detect_smile_or_emotion():
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_smile.xml")
+
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            roi_gray = gray[y:y + h, x:x + w]
+            smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+            if len(smiles) > 0:
+                print("Smile detected")
+        cv2.imshow("Facial Recognition", frame)
+        if cv2.waitKey(10) == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
