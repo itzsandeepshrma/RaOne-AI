2a554abb83345d61e0b7d9b9f91227e291bf1f36
sαɴɖεερ sʜαяɱα <sandeepshrmadev@gmail.com>
2025-05-03 22:55:54 +0530

    Update emoji_visualization.py

--- RaOne/visual_interface/emoji_visualization.py
+++ RaOne/visual_interface/emoji_visualization.py
@@ -1,1 +1,8 @@
-
+def show_emoji(emotion):
+    emojis = {
+        "happy": "😄",
+        "sad": "😢",
+        "angry": "😠",
+        "neutral": "😐"
+    }
+    print(f"[Emoji] Detected emotion: {emotion} {emojis.get(emotion, '')}")
