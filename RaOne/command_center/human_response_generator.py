358787e2da5d9576f8695001ab8c610c0f5e8c03
sαɴɖεερ sʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-05-03 22:46:04 +0530

    Update human_response_generator.py

--- RaOne/command_center/human_response_generator.py
+++ RaOne/command_center/human_response_generator.py
@@ -1,1 +1,21 @@
+import random
+from ..brain.sarcasm_recognition import detect_sarcasm
 
+class HumorResponseGenerator:
+    def __init__(self):
+        self.humor_responses = [
+            "Oh, really? I thought you were being serious!",
+            "Haha, you got me there!",
+            "That was a good one, keep them coming!",
+            "I see what you did there. Nice!",
+            "Oh, I love sarcasm! More, please!"
+        ]
+    
+    def generate_humor_response(self, user_input):
+        """
+        Generates a humorous or sarcastic response based on user input.
+        """
+        if detect_sarcasm(user_input):
+            return random.choice(self.humor_responses)
+        else:
+            return "I don't quite get it, but I'm learning!"
